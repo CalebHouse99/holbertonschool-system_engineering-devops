84d76fb73ae4864c3d13ce711315ba57bcf610ff
CalebHouse99 <calebhouse99@gmail.com>
2022-11-30 17:54:02 -0800

    task 0

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""returns information about employee list progress"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    #employee info
+    emp = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    ).json()
+
+    #employee todo list
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
+    ).json()
+
+    #convert to list obj
+    todo_list = [
+        task.get("title") for task in todos if task.get("completed") is True
+    ]
+
+    #format and print
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            emp.get("name"), len(todo_list), len(todo)
+        ), *todo_list, sep="\n\t "
+    )
