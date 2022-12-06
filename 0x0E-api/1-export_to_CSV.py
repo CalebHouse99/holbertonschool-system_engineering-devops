3b7e195695832eaea205bf55a0f9a0cc4ef44b50
CalebHouse99 <calebhouse99@gmail.com>
2022-12-05 21:58:21 -0800

    tasks 1-3

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Export in CSV format"""
+import csv
+import requests
+import sys
+
+
+def export_to_CSV():
+    """stores into CSV file"""
+    n = sys.argv[1]
+
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(n)
+    url_tasks = ("https://jsonplaceholder.typicode.com/users/{}/todos".
+                 format(n))
+
+    tasks = requests.get(url_tasks).json()
+    user_info = requests.get(url_user).json()
+    employee_name = user_info.get("username")
+
+    with open('{}.csv'.format(n), 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([task.get('userId'), employee_name,
+                             task.get('completed'), task.get('title')])
+
+
+if __name__ == '__main__':
+    export_to_CSV()
--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Export to JSON format"""
+import json
+import requests
+
+
+def everyone_todo_to_JSON():
+    """saves in JSON format"""
+    url = "https://jsonplaceholder.typicode.com/"
+
+    employee_names = requests.get(url + 'users').json()
+    employees_todos = requests.get(url + 'todos').json()
+
+    return_dict = {}
+    all_users_dict = {}
+
+    for employee in employee_names:
+        user_id = employee.get('id')
+        return_dict[user_id] = []
+        all_users_dict[user_id] = employee.get('username')
+
+    for task in employees_todos:
+        employee_tasks_dict = {}
+        user_id = task.get('userId')
+        employee_tasks_dict['username'] = all_users_dict.get(user_id)
+        employee_tasks_dict['task'] = task.get('title')
+        employee_tasks_dict['completed'] = task.get('completed')
+        return_dict.get(user_id).append(employee_tasks_dict)
+
+    with open('todo_all_employees.json', 'w') as JSONFile:
+        json.dump(return_dict, JSONFile)
+
+
+if __name__ == "__main__":
+    everyone_todo_to_JSON()
