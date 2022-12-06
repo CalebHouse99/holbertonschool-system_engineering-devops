66c9c5c662646c143f966c05e6761d810b4004df
CalebHouse99 <calebhouse99@gmail.com>
2022-12-05 21:52:22 -0800

    pycodestyle

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -5,22 +5,22 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    #employee info
+    # employee info
     employee = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     ).json()
 
-    #employee todo list
+    # employee todo list
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
     ).json()
 
-    #convert to list obj
+    # convert to list obj
     todo_list = [
         task.get("title") for task in todos if task.get("completed") is True
     ]
 
-    #format and print
+    # format and print
     print(
         "Employee {} is done with tasks({}/{}):".format(
             employee.get("name"), len(todo_list), len(todos)
